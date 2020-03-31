096e180369017e6f050761bbeef07cc67cd36577
檀陈鑫 <1106901749@qq.com>
2020-03-31 17:47:29 +0800

    3.31

---
+++ elementui/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+	<router-link to="login">11</router-link>
+	<router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+  }
+}
+</script>
+
+<style>
+</style>
